5d66bfdae3855a2a823174092206fd877a76fb2d
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-02-17 22:36:26 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+function App() {
+	return (
+		<>
+			<h2>Aditya Raj Singh</h2>
+			<p>Software Developer</p>
+			<a href="https://www.linkedin.com/in/aditya-singh-653451136/">
+				LinkedIn
+			</a>
+			<br />
+			<a href="https://github.com/Arsh3686">Github</a>
+			<hr />
+			<h3>Exprience</h3>
+			<h4>Software developer at Amazom</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+				vitae error, ducimus ut qui sed eligendi ab repellendus ipsum
+				dignissimos aperiam reprehenderit officiis inventore aliquam
+				consequatur, odio adipisci tenetur a.
+			</p>
+			<hr />
+			<h4>Educator at Geekster</h4>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+				vitae error, ducimus ut qui sed eligendi ab repellendus ipsum
+				dignissimos aperiam reprehenderit officiis inventore aliquam
+				consequatur, odio adipisci tenetur a.
+			</p>
+			<hr />
+			<h4>Skills</h4>
+			<ul>
+				<li>Java</li>
+				<li>JavaScript</li>
+				<li>Css</li>
+				<li>React</li>
+				<li>Node</li>
+			</ul>
+			<h4>Hobbies</h4>
+			<ul>
+				<li>Cricket</li>
+				<li>Singing</li>
+				<li>Acting</li>
+				<li>Lyrics writing</li>
+			</ul>
+		</>
+	);
+}
+
+export default App;
