a26b3945559cda7d663167c061cfd5fd6d9056d1
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-03-04 01:35:47 +0530

    A

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import "./App.css";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+import Store from "./Store";
+export const Context = createContext(Store);
 function App() {
-	const [mark, setMark] = useState("");
-	const handleMark = (e) => {
-		setMark(e.target.value);
+	const ctx = useContext(Context);
+	const [toggler, setToggler] = useState(false);
+	const { toggle, color } = ctx;
+	const toggles = () => {
+		setToggler(!toggler);
 	};
+	useEffect(() => {
+		ctx.toggle = toggler ? "white" : "black";
+		ctx.color = toggler ? "black" : `#6AD5B1`;
+	}, [toggler]);
+
 	return (
-		<div className="main">
-			<textarea
-				cols={100}
-				rows={100}
-				onChange={handleMark}
-				value={mark}
-			/>
-			<ReactMarkdown className="mm" children={mark}></ReactMarkdown>
-		</div>
+		<Context.Provider value={Store}>
+			<div
+				style={{
+					backgroundColor: `${toggle}`,
+					color: `${color}`,
+				}}>
+				<Router>
+					<Nav />
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<Main
+									toggles={toggles}
+									toggle={toggle}
+									color={color}
+								/>
+							}
+						/>
+					</Routes>{" "}
+				</Router>
+			</div>
+		</Context.Provider>
 	);
 }
 
---
+++ src/Store.jsx
@@ -0,0 +1,5 @@
+const Store = {
+	toggle: "white",
+	color: "black",
+};
+export default Store;
