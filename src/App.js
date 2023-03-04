dc63fc801279145b3a6eec4e687a9c285e112954
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-03-04 10:26:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import Main from "./components/Main";
-import API_ENDPOINT, { API_ID, API_KEY } from "../src/Store";
+import API_KEY from "../src/Store";
 
 function App() {
 	const [collector, setCollectore] = useState([]);
 	const [inputCollector, setInputCollector] = useState("");
-	const [queryCollector, querySetter] = useState("");
+	const [queryCollector, querySetter] = useState("Iron");
 
 	useEffect(() => {
 		const fetchFun = async () => {
-			const { hits } = await (
+			const { Search } = await (
 				await fetch(
-					`https://api.edamam.com/api/recipes/v2?type=public&q=${queryCollector}&app_id=${API_ID}&app_key=${API_KEY}`
+					`https://www.omdbapi.com/?s=${queryCollector}&apikey=${API_KEY}`
 				)
 			).json();
-			setCollectore([...hits]);
+			console.log(Search);
+			setCollectore([...Search]);
 		};
 		fetchFun();
 	}, [queryCollector]);
@@ -25,25 +25,34 @@ function App() {
 	};
 	return (
 		<>
+			<div className="hooked">Hooked</div>
 			<input
 				value={inputCollector}
 				onChange={(e) => setInputCollector(e.target.value)}
-				placeholder="Search your recipie"
+				placeholder="Search your Movies"
 			/>
 			<button onClick={submit}>Submit</button>
 			<hr />
-			<ul style={{ display: "flex", flexWrap: "wrap", width: "100vw" }}>
-				{collector.map((e) => {
+			<ul
+				style={{
+					display: "flex",
+					flexWrap: "wrap",
+					width: "100vw",
+					justifyContent: "center",
+				}}>
+				{collector.map((e, id) => {
 					return (
 						<div
+							key={id}
 							style={{
 								display: "flex",
 								flexDirection: "column",
 								alignItems: "center",
-								width: "300px",
+								width: "200px",
+								height: "300px",
 							}}>
-							<img width={100} src={e.recipe.image} alt="" />
-							<p>Recipe_Name: {e.recipe.label}</p>
+							<img width={150} src={e.Poster} alt={e.Title} />
+							<p>Movie_Name: {e.Title}</p>
 						</div>
 					);
 				})}
--- src/Store.jsx
+++ src/Store.jsx
@@ -1,2 +1,2 @@
-export const API_ID = "2ad1d97b";
-export const API_KEY = "372c4c5c9f1693fb4eb3c155ae2f5cb8";
+const API_KEY = "892cbcde";
+export default API_KEY;
