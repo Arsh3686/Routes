70c71f8aef62270ce1f453fabd9a0b784154ba8e
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-03-12 10:51:14 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import "./style/About.css";
 const About = () => {
 	return (
 		<div className="about">
-			<img src="./images/About.jpg" alt="" />
+			<img src="https://www.urbansciencemep.com/_next/static/images/about-1-fcabb8ead6b6f55150f09d642c983f7f.jpg" alt="" />
 		</div>
 	);
 };
