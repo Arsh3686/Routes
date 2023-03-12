4c9172ec4e80e4f5dc1ed9e4d40bc1eb9b128c5a
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-03-12 10:50:17 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ import "./style/Project.css";
 const Project = () => {
 	return (
 		<div className="project">
-			<img src="./images/Project.jpg" alt="" />
+			<img src="https://www.urbansciencemep.com/_next/static/images/about-1-fcabb8ead6b6f55150f09d642c983f7f.jpg" alt="" />
 		</div>
 	);
 };
